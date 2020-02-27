12120eaf5de11e76c1b3553a483b63b65802ee1b
Josh Mu <mu@joshmu.com>
2020-02-27 11:40:51 +1000

    logItem, react-moment, preloader

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Preloader = () => {
+  return (
+    <div className="progress">
+      <div className="indeterminate indigo lighten-3"></div>
+    </div>
+  )
+}
+
+export default Preloader
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import LogItem from './LogItem'
+import Preloader from '../layout/Preloader'
 
 const Logs = () => {
   const [logs, setLogs] = useState([])
@@ -16,21 +18,17 @@ const Logs = () => {
     setLoading(false)
   }
 
-  if (loading) return <h4>Loading...</h4>
+  if (loading) return <Preloader />
 
   return (
     <ul className="collection with-header">
-      <li className="collection-header">
-        <h4>Logs</h4>
+      <li className="collection-header center">
+        <h4>System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
         <p className="center">No logs showing.</p>
       ) : (
-        logs.map(log => (
-          <li key={log.id} className="collection-item">
-            {log.message}
-          </li>
-        ))
+        logs.map(log => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   )
