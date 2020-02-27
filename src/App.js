c31d4da800ae53848ca1a0bcc85ccfe9ba872b6f
Josh Mu <mu@joshmu.com>
2020-02-27 10:34:00 +1000

    react initial with materialize-css setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import '../node_modules/materialize-css/dist/css/materialize.min.css'
+import M from '../node_modules/materialize-css/dist/js/materialize.min.js'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    M.AutoInit()
+    // eslint-disable-next-line
+  }, [])
+  return <div className="App">Hello</div>
 }
 
-export default App;
+export default App
