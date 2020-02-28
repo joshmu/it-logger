ec2eacb9ae43cbdf89485c6e9d2b425acbae1417
Josh Mu <mu@joshmu.com>
2020-02-28 16:17:16 +1000

    logReducer

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  logs: [],
+  current: null,
+  loading: false,
+  error: null
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
